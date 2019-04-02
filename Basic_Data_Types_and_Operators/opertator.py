efbbb45efa4381d49e6489099bb3371534d6d75c
heeewo <esc5411@naver.com>
2019-04-02 14:32:58 +0900

    Add opertator.py
    Signed-off-by: heeewo <esc5411@naver.com>

---
+++ Basic_Data_Types_and_Operators/opertator.py
@@ -0,0 +1,13 @@
+# 1. 논리연산자에 대해 배운 것을 떠올리며 다음 세 가지 결과를 출력합니다.
+print(False and False)
+print(True or True)
+print(not False)
+# 2. 관계연산자에 대해 배운 것을 떠올리며 다음 내용을 순서대로 코딩합니다.
+c = 10 ; d = 4
+print(c == 10)
+print(c == d)
+print(c != 4)
+print(d >= 0)
+# 3. 비트 연사자에 대해 배운 것을 떠올리며 다음 내용을 코딩합니다.
+print(10110 & 10011)
+print(10010 | 10101)
