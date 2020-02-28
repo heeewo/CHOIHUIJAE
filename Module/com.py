a5fbc94a5f6db64dd503a021ff05b4dbf60a2923
heeewo <esc5411@naver.com>
2020-02-28 20:23:23 +0900

    Add com.py Del win32com.py

---
+++ Module/com.py
@@ -0,0 +1,9 @@
+import win32com.client
+
+excel = win32com.client.Dispatch("Excel.Application")
+excel.Visible = True
+wb = excel.Workbooks.Add()
+ws = wb.Worksheets("Sheet1")
+ws.Cells(1, 1).Value = "hello world"
+wb.SaveAs('c:\\Users\\최희준\\Desktop\\test.xlsx')
+excel.Quit()
--- Module/win32com.py
+++
@@ -1,3 +0,0 @@
-import win32com.client
-explore = win32com.client.Dispatch("Word.Application")
-explore.Visible = True
