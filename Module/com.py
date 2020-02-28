5db70b21fac988f51bbfd6cc75574296997c9796
heeewo <esc5411@naver.com>
2020-02-28 21:05:36 +0900

    modify com.py

--- Module/com.py
+++ Module/com.py
@@ -2,8 +2,13 @@ import win32com.client
 
 excel = win32com.client.Dispatch("Excel.Application")
 excel.Visible = True
-wb = excel.Workbooks.Add()
-ws = wb.Worksheets("Sheet1")
-ws.Cells(1, 1).Value = "hello world"
-wb.SaveAs('c:\\Users\\최희준\\Desktop\\test.xlsx')
+wb = excel.Workbooks.Open('C:\\Users\\최희준\\Desktop\\test.xlsx')
+ws = wb.Activesheet
+ws2 = excel.Worksheets.Add()
+ws2 = wb.worksheet('Sheet2')
+ws2.name = "shit"
+ws.Range('A2:B3').Value = "PER"
+ws.Range('A4:B5,A7:B8').Value = "PBR"
+ws.Range('A7:B8').Interior.ColorIndex = 10
+print(ws.Cells(1,1).Value)
 excel.Quit()
