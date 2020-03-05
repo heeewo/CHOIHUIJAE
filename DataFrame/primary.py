332aedc1e114b414ddfd91f50a80cdb492b50953
heeewo <esc5411@naver.com>
2020-03-05 19:02:51 +0900

    Add DataFrame (primary.py)

---
+++ DataFrame/primary.py
@@ -0,0 +1,9 @@
+from pandas import Series, DataFrame
+
+daeshin = {'open':  [11650, 11100, 11200, 11100, 11000],
+           'high':  [12100, 11800, 11200, 11100, 11150],
+           'low' :  [11600, 11050, 10900, 10950, 10900],
+           'close': [11900, 11600, 11000, 11100, 11050]}
+
+daeshin_day = DataFrame(daeshin)
+print(daeshin_day)
